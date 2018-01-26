cd13de0aff928edeaa762fdc0266c7f4e327248b
ramonalves <ramon.vasco91@gmail.com>
2018-01-26 15:33:46 -0200

    Correção de menu

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
           </ul>
           <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
           <div class="collapse navbar-collapse navbar-ex1-collapse">
-              <ul class="nav navbar-nav side-nav">
+              <ul class="nav navbar-nav">
                   <li>
                       <a href="#/categories"><i class="fa fa-fw fa-paper-plane-o"></i> Categorias</a>
                   </li>
